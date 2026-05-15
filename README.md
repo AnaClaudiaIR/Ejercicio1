6770d10d0116a54a7f5507c3451261712b71235b
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-05-15 10:02:17 +0200

    subir2

---
+++ README.md
@@ -0,0 +1,1 @@
+Ejemplo de un botón en JavaFX.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,34 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+//import java.awt.*;
+//import java.awt.TextField; --> No se necesitan estos imports
+import javafx.scene.control.*;
+
+import java.util.Optional;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Botón"); //Nombre ventana
+        Label label = new Label("Botón ejemplo"); //Mensaje información
+        Button boton = new Button("Haz clic aquí"); //Botón
+
+        VBox vBox = new VBox(label,boton); //Caja donde meter los elementos
+
+        Scene scene = new Scene(vBox,200,100); //Ventana
+        primaryStage.setScene(scene); //Colocar la ventana en la escena
+        primaryStage.show(); //Mostrar la escena
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
